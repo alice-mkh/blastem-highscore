8c4e7935a0b488eea4329ae5d689446437c34c9f
Michael Pavone <pavone@retrodev.com>
2022-11-24 00:54:18 -0800

    Fix settings save path of TMSS ROM setting

--- nuklear_ui/blastem_nuklear.c
+++ nuklear_ui/blastem_nuklear.c
@@ -2177,7 +2177,7 @@ void view_bios_settings(struct nk_context *context)
 		uint32_t desired_width = context->style.font->height * 10;
 		nk_layout_row_static(context, context->style.font->height, desired_width, 2);
 		static const char* exts[] = {"md", "bin", "smd"};
-		settings_path(context, "TMSS ROM", "ui\0save_path\0", "tmss.md", exts, 3);
+		settings_path(context, "TMSS ROM", "system\0tmss_path\0", "tmss.md", exts, 3);
 		settings_path(context, "US CD BIOS", "system\0scd_bios_us\0", "cdbios.md", exts, 3);
 		settings_path(context, "JP CD BIOS", "system\0scd_bios_jp\0", "cdbios.md", exts, 3);
 		settings_path(context, "EU CD BIOS", "system\0scd_bios_eu\0", "cdbios.md", exts, 3);
